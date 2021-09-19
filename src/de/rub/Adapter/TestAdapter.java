9d79dba2b856878d8856fc51fd4512e974841af3
Simon Tintrop <simon.tintrop@googlemail.com>
2021-09-19 12:17:35 +0200

    korrektur

--- src/de/rub/Adapter/TestAdapter.java
+++ src/de/rub/Adapter/TestAdapter.java
@@ -1,16 +1,15 @@
 package de.rub.Adapter;
 
-public class TestKlasse {
+public class TestAdapter {
 
     public static void main(String[] args) {
-        USAddressAdapter USAddress = new USAddressAdapter();
         AddressValidator validator = new DEAddress();
         boolean valid = false;
         valid = validator.isGueltig("Universitätsstr. 150", "44801", "NRW");
         System.out.println(valid);
         valid = validator.isGueltig("Universitätsstr. 150", "44801", "NW");
         System.out.println(valid);
-        validator = USAddress;
+        validator = new USAddressAdapter();
         valid = validator.isGueltig("1600 Pennsylvania Ave NW, Washington", "20500", "DC");
         System.out.println(valid);
     }
